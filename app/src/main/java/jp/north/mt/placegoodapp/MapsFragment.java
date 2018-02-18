f3a08efc5452959f8494736308246ca9e16eddfa
KITA <yk.june13@gmail.com>
2018-02-18 20:15:21 +0900

    経度、緯度の取得（途中_3）

--- app/src/main/java/jp/north/mt/placegoodapp/MapsFragment.java
+++ app/src/main/java/jp/north/mt/placegoodapp/MapsFragment.java
@@ -6,10 +6,12 @@ import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.location.LocationProvider;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -21,7 +23,9 @@ import com.google.android.gms.maps.SupportMapFragment;
 
 import java.util.Locale;
 
-public class MapsFragment extends Fragment implements OnMapReadyCallback {
+import static android.content.Context.LOCATION_SERVICE;
+
+public class MapsFragment extends Fragment implements OnMapReadyCallback, LocationListener {
 
     private GoogleMap mMap;
 
@@ -47,53 +51,83 @@ public class MapsFragment extends Fragment implements OnMapReadyCallback {
         mMap = googleMap;
 
         //LocationManagerの取得
-        LocationManager locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+        LocationManager locationManager = (LocationManager) getActivity().getSystemService(LOCATION_SERVICE);
         //パーミッション（※Fragmentのため、thisではなく、getActivity()で親のActivityを呼び出し）
         if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
                 && ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
             //現在地の座標を取得
             mMap.setMyLocationEnabled(true);
         }
-//        Location myLocate = locationManager.getLastKnownLocation("gps");
     }
 
-    LocationListener locationListener = new LocationListener() {
-        @Override
-        public void onLocationChanged(Location location) {
-            double latitude = location.getLatitude();
-            double longitude = location.getLongitude();
-            textView.setText(String.format(Locale.US, "%f,%f", latitude, longitude));
-        }
-
-        @Override
-        public void onStatusChanged(String s, int i, Bundle bundle) {
-
-        }
-
-        @Override
-        public void onProviderEnabled(String s) {
-
-        }
-
-        @Override
-        public void onProviderDisabled(String s) {
-
-        }
-    };
-    LocationManager locationManager;
+    private LocationManager mLocationManager;
 
 //    @Override
-//    public void onStart() {
-//        super.onStart();
-//        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-//        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
-//    }
+//    public void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.fragment_maps);
 //
-//    @Override
-//    public void onStop() {
-//        super.onStop();
-//        locationManager.removeUpdates(locationListener);
+//        mLocationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
 //    }
 
-}
+    @Override
+    public void onResume() {
+        if (mLocationManager != null) {
+            if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                    && ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
+            mLocationManager.requestLocationUpdates(
+                    LocationManager.GPS_PROVIDER,
+//                LocationManager.NETWORK_PROVIDER,
+                    0,
+                    0,
+                    this);
+        }
 
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        if (mLocationManager != null) {
+            mLocationManager.removeUpdates(this);
+        }
+
+        super.onPause();
+    }
+
+
+    @Override
+    public void onLocationChanged(Location location) {
+        final float longitude = Double.valueOf(location.getLongitude()).floatValue();
+        final float latitude = Double.valueOf(location.getLatitude()).floatValue();;
+
+        Log.e("MapFragment", String.format("lon:%f lat:%f", longitude, latitude));
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        switch (status) {
+            case LocationProvider.AVAILABLE:
+                Log.v("Status", "AVAILABLE");
+                break;
+            case LocationProvider.OUT_OF_SERVICE:
+                Log.v("Status", "OUT_OF_SERVICE");
+                break;
+            case LocationProvider.TEMPORARILY_UNAVAILABLE:
+                Log.v("Status", "TEMPORARILY_UNAVAILABLE");
+                break;
+        }
+    }
+}
